7a46fcec4123f1d6ec2f13cbbc619f7a1e8fdca1
MJan79 <104592315+MJan79@users.noreply.github.com>
2022-05-15 16:36:02 -0700

    Add files via upload
    My Random Password Generator (in progress)

---
+++ Random_Pwd_Gen.py
@@ -0,0 +1,28 @@
+#Password Generator Project
+import random
+letters = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z']
+numbers = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9']
+symbols = ['!', '#', '$', '%', '&', '(', ')', '*', '+']
+#User input
+print("Welcome to the Password Generator!")
+nr_letters= int(input("How many letters would you like in your password?\n")) 
+nr_symbols = int(input(f"How many symbols would you like?\n"))
+nr_numbers = int(input(f"How many numbers would you like?\n"))
+#Password tables
+pwd_letters=[]
+pwd_symbols=[]
+pwd_numbers=[]
+pwd=[]
+#Combine tables and shuffle items
+pwd_letters=random.sample(letters,nr_letters)
+pwd_symbols=random.sample(symbols,nr_symbols)
+pwd_numbers=random.sample(numbers,nr_numbers)
+pwd = pwd_letters + pwd_symbols + pwd_numbers
+random.shuffle(pwd)
+#Print password using for loop
+password=""
+for char in pwd:
+  password += char
+print(f"Your password: {password}")
+
+
